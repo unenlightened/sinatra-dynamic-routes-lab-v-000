272064a2d6e8f0561ec912d5b1bbc09560415c55
Chely Ho <adept_inept@yahoo.com>
2018-03-13 15:05:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,12 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  
+
   get '/reversename/:name' do
     "#{params[:name].reverse}"
   end
 
   get '/square/:number' do
     "#{params[:number].to_i ** 2}"
+  end
+
+  get '/say/:number/:phrase' do
+    params[:number].to_i.times do
+      "params[:phrase]"
+    end
   end
 end
