adc79256c238cf1513d4cf73c38d00b89b35b68a
Chely Ho <adept_inept@yahoo.com>
2018-03-13 15:07:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,4 +15,8 @@ class App < Sinatra::Base
       "params[:phrase]"
     end
   end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1] params[:word2] params[:word3] params[:word4] params[:word5] }"
+  end
 end
